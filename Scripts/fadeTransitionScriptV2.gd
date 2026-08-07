a239e9999ff65d3d088de6e8c2a9df3be3cd260c
Sima Sandouk <111926705+simasandouk@users.noreply.github.com>
2026-08-08 01:13:27 +0300

    integrating

--- Scripts/fadeTransitionScriptV2.gd
+++ Scripts/fadeTransitionScriptV2.gd
@@ -1,9 +1,11 @@
 extends CanvasLayer
 
 @onready var animPlayer: AnimationPlayer = $fadeTransition/AnimationPlayer
-@onready var timer: Timer = $fadeTransition/Timer
+@onready var sceneTransitionTimer: Timer = $fadeTransition/sceneTransitionTimer1
 var sceneToLoad
-@onready var timer2: Timer = $fadeTransition/Timer2
+@onready var sceneTransitionTimer2: Timer = $fadeTransition/sceneTransitionTimer2
+@onready var blinkTimer: Timer = $fadeTransition/blinkTimer1
+@onready var blinkTimer2: Timer = $fadeTransition/blinkTimer2
 
 
 # Called when the node enters the scene tree for the first time.
@@ -19,9 +21,9 @@ func _process(_delta: float) -> void:
 
 func loadScene(sceneName: String): 
 	show()
+	animPlayer.speed_scale = 1.0
 	sceneToLoad = sceneName
-	print(timer.wait_time)
-	timer.start()
+	sceneTransitionTimer.start()
 	animPlayer.play("fadeIn")
 
 
@@ -29,9 +31,27 @@ func _on_timer_timeout() -> void:
 	print("called")
 	get_tree().change_scene_to_file(sceneToLoad)
 	animPlayer.play("fadeOut")
-	timer2.start()
+	sceneTransitionTimer2.start()
 
 
 func _on_timer_2_timeout() -> void:
 	print("hidden")
 	hide()
+
+func blink(blinkDuration: float, fadeDuration: float): 
+	show()
+	animPlayer.speed_scale = 1.0 / fadeDuration
+	blinkTimer.wait_time = fadeDuration
+	blinkTimer2.wait_time = blinkDuration - 2 * fadeDuration
+	blinkTimer.start()
+	animPlayer.play("fadeIn")
+
+
+func _on_blink_timer_1_timeout() -> void:
+	blinkTimer2.start()
+	print(blinkTimer2.wait_time)
+
+
+func _on_blink_timer_2_timeout() -> void:
+	print("play fadeout")
+	animPlayer.play("fadeOut")
--- Scripts/logic_manager.gd
+++ Scripts/logic_manager.gd
@@ -1,24 +1,38 @@
 extends Node
 
 @export var lane_count: int = 3
-@export var street_width: float = 9.0
+@export var street_width: float = 500
 var lane_width: float
 @export var start_lane: int = 1
 @export var switch_dur: float = 0.12
 @export var switch_lock: bool = true
-
 @onready var player: Node = %"Player"
 @onready var UIManagar: Node = %"UI Manager"
-var paused = false
-var score = 0.0 # time survived
+var paused: bool = false
+var score: float = 0.0 # time survived
+var multiplyer: float = 1.0
+@export var initialEnergy: int  = 10
+@export var energyAfterLongBlink: int = 10
+var energy: int
+var timer = 0.0
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	lane_width = street_width / float(lane_count)
+	energy = initialEnergy
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
+	timer += _delta
+	score += (multiplyer * _delta)
+	if int(timer) != int(timer - _delta): 
+		energy -= 1
+		if energy == 0:
+			energy = energyAfterLongBlink
+			FadeTransitionSceneV2.blink(2.0, 0.5)
+	
 	if Input.is_action_just_pressed("pause"):
 		if not paused:
 			pause_game()
@@ -33,5 +47,5 @@ func unpause_game() -> void:
 
 func gameOver():
 	print("GAME OVER")
-	SilentWolf.Scores.save_score(Global.playerName, score)
+	SilentWolf.Scores.save_score("Global.playerName", score)
 	score = 0
