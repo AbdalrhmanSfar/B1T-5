d99644d2a326f043c948516f4927ba26314075ff
AbdalrhmanSfar <abdalrhmansfar@gmail.com>
2026-08-08 00:45:46 +0300

    blinking

--- Scripts/logic_manager.gd
+++ Scripts/logic_manager.gd
@@ -4,14 +4,28 @@ extends Node
 @onready var UIManagar: Node = %"UI Manager"
 var paused = false
 var score = 0.0 # time survived
+var multiplyer = 1.0
+@export var initialEnergy = 10
+@export var energyAfterLongBlink = 10
+var energy: int
+var timer = 0.0
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	energy = initialEnergy
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
+	timer += _delta
+	score += (multiplyer * _delta)
+	if int(timer) != int(timer - _delta): 
+		energy -= 1
+		if energy == 0:
+			energy = energyAfterLongBlink
+			FadeTransitionSceneV2.blink(2.0, 0.5)
+	
 	if Input.is_action_just_pressed("pause"):
 		if not paused:
 			pause_game()
@@ -26,5 +40,5 @@ func unpause_game() -> void:
 
 func gameOver():
 	print("GAME OVER")
-	SilentWolf.Scores.save_score(Global.playerName, score)
+	SilentWolf.Scores.save_score("Global.playerName", score)
 	score = 0
